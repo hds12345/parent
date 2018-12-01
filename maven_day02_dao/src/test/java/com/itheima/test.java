d84d3d31f782daef38e1c4da82c8af77bc0c6df3
hds <hds@qq.com>
2018-12-01 15:00:02 +0800

    添加工程

---
+++ maven_day02_dao/src/test/java/com/itheima/test.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello svn");
+        System.out.println("hello svn");
+        System.out.println("hello svn");
+        int a = 100;
+        int b = 200;
+    }
+}
